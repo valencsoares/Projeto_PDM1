c5677f78ac6e4dc37f290c5bf6e00a2df4a7842c
valencsoares <pvcampossoares558@gmail.com>
2024-02-28 14:30:51 -0400

    Mudado a cor do fundo e da letra

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>thi thi!</Text>
+      <Text style={styles.text}>thi thi!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'white',
+  }
 });

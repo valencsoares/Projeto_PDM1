856331b0bf2da65c67d7bac31066e64f7b2014de
valencsoares <pvcampossoares558@gmail.com>
2024-03-06 14:30:43 -0400

    Feito a função para retornar a primeira letra de cada nome (nome e sobrenome) da pessoa

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const getNomeCompleto = (primeiro, segundo, terceiro) => {
+  return primeiro.charAt(0) + " " + segundo.charAt(0) + " " + terceiro.charAt(0) + "."
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>thi thi!</Text>
+      <Text style={styles.text}>Nome completo: {getNomeCompleto("Thiago", "Fernandes", "Zanon")} </Text>
       <StatusBar style="auto" />
     </View>
   );
